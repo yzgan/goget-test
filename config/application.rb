bdbd30b749545e2ae863ba4888fa0b43df5b8388
Gan Yi Zhong <ganyizhong@gmail.com>
2021-07-15 21:04:57 +0800

    rails admin setup

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,10 @@ module GogetTest
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Session::CookieStore, {:key=>"_goget_test_session"}
   end
 end
