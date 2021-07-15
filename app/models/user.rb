c54fcefc8081288d13eba640e793e422eeaf4aa5
Gan Yi Zhong <ganyizhong@gmail.com>
2021-07-15 10:34:34 +0800

    validate user name presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+
   def generate_jwt
     JWT.encode({ id: id, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
   end
